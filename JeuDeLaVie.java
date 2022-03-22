3dfc4b8ec2ed73f69d63ccef3344f258c20259c2
DrissGIANT <82338884+DrissGIANT@users.noreply.github.com>
2022-03-22 10:43:40 +0100

    Update JeuDeLaVie.java

--- JeuDeLaVie.java
+++ JeuDeLaVie.java
@@ -1,4 +1,5 @@
-import java.util.Hashtable;
+package JDL;
+import java.lang.Thread;
 import java.util.Random;
 
 public class JeuDeLaVie {
@@ -11,13 +12,18 @@ public class JeuDeLaVie {
 		tab = new int[100][100];
 		 for (int i = 0;i < tab.length; i++) {
 	         for (int j = 0;j < tab[i].length;j++) {
-	        		 tab[i][j] = 0;
-	      }
-		}
-		//tab[10][8]=1;
-		//tab[9][2]=1;
-		 //tab[8][7]=1;
-		 //tab[12][4]=1;
+	        	 int min_val = 10;
+	             int max_val = 100;
+	             Random ran = new Random();
+	             int x = ran.nextInt(max_val) + min_val;
+	             	if(x%2==0) {
+	             		tab[i][j] = 1;
+	             	}
+	             	else {
+	             		tab[i][j] = 0;
+	             	}
+	         }
+		 }
 	}
 	
 	//Affiche le tableau
@@ -30,6 +36,22 @@ public class JeuDeLaVie {
 	      }
 	}
 	
+	public void affichTab2(String a, String b) {
+		String[][] tabb = new String[100][100];
+		 for (int i = 0;i < tab.length; i++) {
+	         for (int j = 0;j < tab[i].length;j++) {
+	        	 if(tab[i][j]==1) {
+	        		 tabb[i][j]=a;
+	        		 System.out.print(tabb[i][j] + " ");
+	        	 }else {
+	        		 tabb[i][j]=b;
+	        		 System.out.print(tabb[i][j] + " ");
+	        	 }
+	         }
+	         System.out.println();
+		 }
+		
+	}
 	//Retourne la valeur
 	public int valeur_case(int i, int j) {
 		return tab[i][j];
@@ -72,6 +94,37 @@ public class JeuDeLaVie {
 			somme=tab[i-1][j-1]+tab[i-1][j]+tab[i-1][j+1]+tab[i][j+1]+tab[i+1][j+1]+tab[i+1][j]+tab[i+1][j-1]+tab[i][j-1];
 		}
 		return somme;
+	}
+	public int resultat(int i, int j) {
+		if( total_voisins(i,j)>=3) {
+			return 1;
+		}else {
+			return 0;
+		}
+	}
+	public void tour() {
+		for (int i = 0;i < tab.length; i++) {
+	         for (int j = 0;j < tab[i].length;j++) {
+	        		 tab[i][j] = resultat(i,j);
+	      }
+		}
+	}
+	public void run(int nb_tours,int delai){
+		for(int i=0;i<nb_tours;i++) {
+			int [][] tabi=tab;
+				tour();
+				try {
+					Thread.sleep(delai);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				affichTab();
+				//Arrêt lorsqu'aucun changement n'est détecté.
+				if(tabi==tab) {
+					break;
+				}
+			}
 	}
 }
 
