2a3476c77ffba7bfbd4e534856f14126fa0147fe
djamelboubred <djamelboubred.saclay@gmail.com>
2022-03-24 16:03:45 +0100

    brouillon
    ajout de la notion de probabilité
    brouillon d'idée potentiel d'amélioration

---
+++ Population.java
@@ -0,0 +1,45 @@
+/*public class Population{
+	public static void main(String[] args){
+		System.out.println(pop1.contamine);
+	}
+	boolean vaccine;
+	boolean contamine;
+	int incubation; // temps d'incubation necessaire pour commencé à être contagieux 0 
+	//boolean masque;
+	Population[] tab = new Population [1];
+	Population(){
+		Random ran  =  new Random();
+		int x = ran.nextInt(4);
+		switch(a)
+		{
+			case 1
+		}
+			this.vaccine = true;
+			this.contamine = true;
+		}	
+		else {
+			this.vaccine = false;
+			this.contamine = false;
+		}	 
+	}
+	Population pop1 = new Population(4);
+	
+}
+*/
+
+// méthode pour appliquer un risque 
+import java.util.Random;
+class Population{
+
+	public static void main(String[] args)
+	{
+		for (int i=0;i<20;i++){
+			Random ran = new Random();
+	    	int x = ran.nextInt(probabilte);
+			System.out.println(x);
+		}
+		
+		
+	}
+
+}
